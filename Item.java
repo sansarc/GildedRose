dbd3c57669185eaeaf964df3f41be8e88504ef5a
sansarc <sansevieroarcangelo@gmail.com>
2024-10-23 10:19:02 +0200

    For loop fix and subclasses implementation.

--- Item.java
+++ Item.java
@@ -19,3 +19,21 @@ public class Item {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
 }
+
+class AgedBrieItem extends Item {
+    AgedBrieItem(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+}
+
+class BackstagePassesItem extends Item {
+    BackstagePassesItem(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+}
+
+class SulfurasItem extends Item {
+    SulfurasItem(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+}
