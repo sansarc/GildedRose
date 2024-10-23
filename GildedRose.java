cdcf97b30285e932f278301e02171104a3175dd1
sansarc <sansevieroarcangelo@gmail.com>
2024-10-23 10:48:02 +0200

    First attempt to work around magic numbers sparse.

--- GildedRose.java
+++ GildedRose.java
@@ -2,7 +2,13 @@ package com.gildedrose;
 
 class GildedRose {
     Item[] items;
-
+    // I'll add it here since I'm not supposed to modify the Item class
+    final int GENERAL_MAX_QUALITY = 50; 
+    final int MIN_QUALITY = 0;
+    final int LEGENDARY_MAX_QUALITY = 80;
+    final int SELLIN_BACKSTAGEPASSES_TRIPLE = 5;
+    final int SELLIN_BACKSTAGEPASSES_DOUBLE = 10;
+    
     public GildedRose(Item[] items) {
         this.items = items;
     }
@@ -11,24 +17,24 @@ class GildedRose {
         for (Item item : items) {
             if (!item.name.equals("Aged Brie")
                     && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality > 0) {
+                if (item.quality > MIN_QUALITY) {
                     if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                         item.quality = item.quality - 1;
                     }
                 }
             } else {
-                if (item.quality < 50) {
+                if (item.quality < GENERAL_MAX_QUALITY) {
                     item.quality = item.quality + 1;
 
                     if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
+                        if (item.sellIn <= SELLIN_BACKSTAGEPASSES_DOUBLE) {
+                            if (item.quality < GENERAL_MAX_QUALITY) {
                                 item.quality = item.quality + 1;
                             }
                         }
 
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
+                        if (item.sellIn <= SELLIN_BACKSTAGEPASSES_TRIPLE) {
+                            if (item.quality < GENERAL_MAX_QUALITY) {
                                 item.quality = item.quality + 1;
                             }
                         }
@@ -40,19 +46,19 @@ class GildedRose {
                 item.sellIn = item.sellIn - 1;
             }
 
-            if (item.sellIn < 0) {
+            if (item.sellIn < MIN_QUALITY) {
                 if (!item.name.equals("Aged Brie")) {
                     if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.quality > 0) {
+                        if (item.quality > MIN_QUALITY) {
                             if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                                 item.quality = item.quality - 1;
                             }
                         }
                     } else {
-                        item.quality = item.quality - item.quality;
+                        item.quality = MIN_QUALITY;
                     }
                 } else {
-                    if (item.quality < 50) {
+                    if (item.quality < GENERAL_MAX_QUALITY) {
                         item.quality = item.quality + 1;
                     }
                 }
